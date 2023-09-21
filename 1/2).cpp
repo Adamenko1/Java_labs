cb208826c85e34b18344c88b7ff5ad099c98840a
Adamenko1 <adamenko2003@gmail.com>
2023-09-21 03:11:55 +0200

    1-ый пункт 1-ой лабораторной

---
+++ 1/2).cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+int sum(std::vector <int> numbers)
+{
+    int s = 0;
+    int sum = 0;
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        auto test = numbers[i];
+        if (s == 0)
+        {
+            sum = sum + numbers[i];
+            s++;
+        }
+        else
+        {
+            sum = sum - numbers[i];
+            s--;
+        }
+    }
+    return sum;
+}
+
+int main()
+{
+    int n, s;
+    std::cin >> n;
+    std::vector <int> numbers(n);
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> numbers[i];
+    }
+    std::cout << sum(numbers);
+}
